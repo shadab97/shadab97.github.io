dcb6d502fe4dbda069367be5920ddb4ddce242fb
Shadab Ali <31549741+shadab97@users.noreply.github.com>
2018-12-24 03:46:44 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@
 // Make sure sw are supported
   if ('serviceWorker' in navigator) {
      navigator.serviceWorker
-        .register('/shadab97.github.io/sw.js', {scope: '/shadab97.github.io/'})
+        .register('/shadab97.github.io/sw.js', scope: '/shadab97.github.io/')
         .then(reg => console.log('Service Worker: Registered (Pages)'))
         .catch(err => console.log(`Service Worker: Error: ${err}`));
   }
