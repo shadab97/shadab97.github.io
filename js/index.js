97460fb60c5c4abe3783bee55532ebf00f8bb52f
Shadab Ali <31549741+shadab97@users.noreply.github.com>
2018-12-24 02:33:57 +0530

    minor err

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@
   if ('serviceWorker' in navigator) {
       window.addEventListener('load', () => {
           navigator.serviceWorker
-              .register('../sw.js')
+              .register('/sw.js')
               .then(reg => console.log('Service Worker: Registered (Pages)'))
               .catch(err => console.log(`Service Worker: Error: ${err}`));
       });
