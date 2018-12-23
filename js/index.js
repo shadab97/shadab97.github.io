f927db2ee5de9ceeb62cca2ec3d4e3b4cee2d925
Shadab Ali <31549741+shadab97@users.noreply.github.com>
2018-12-24 04:06:04 +0530

    del sw

--- js/index.js
+++ js/index.js
@@ -1,13 +1,6 @@
   $(document).ready(function () {
       $('.button-collapse').sideNav();
       $('.tooltipped').tooltip();
-      
+
 
   });
-// Make sure sw are supported
-  if ('serviceWorker' in navigator) {
-     navigator.serviceWorker
-        .register('/shadab97.github.io/sw.js', {scope: '/shadab97.github.io/'})
-        .then(reg => console.log('Service Worker: Registered (Pages)'))
-        .catch(err => console.log(`Service Worker: Error: ${err}`));
-  }
