625833bdae8bebe92934b48a4ee66a7c12c00b43
Shadab Ali <31549741+shadab97@users.noreply.github.com>
2018-12-24 03:25:20 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,13 @@
   $(document).ready(function () {
       $('.button-collapse').sideNav();
       $('.tooltipped').tooltip();
-      // Make sure sw are supported
+      
 
   });
+// Make sure sw are supported
   if ('serviceWorker' in navigator) {
-         window.addEventListener('load', () => {
-              navigator.serviceWorker
-                  .register('/shadab97.github.io/sw.js', {scope: '/shadab97.github.io/'})
-                  .then(reg => console.log('Service Worker: Registered (Pages)'))
-                  .catch(err => console.log(`Service Worker: Error: ${err}`));
-         });
+     navigator.serviceWorker
+        .register('/shadab97.github.io/sw.js', {scope: '/shadab97.github.io/'})
+        .then(reg => console.log('Service Worker: Registered (Pages)'))
+        .catch(err => console.log(`Service Worker: Error: ${err}`));
   }
