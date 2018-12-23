e81b2bf0b064c6b996cdcff6f0bffe7bf6462b81
Shadab Ali <31549741+shadab97@users.noreply.github.com>
2018-12-24 04:21:20 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,1 @@
-  $(document).ready(function () {
-      $('.button-collapse').sideNav();
-      $('.tooltipped').tooltip();
 
-
-  });
-<<<<<<< HEAD
-=======
-
->>>>>>> e3cb944e6bd2d424475c67cb8cc1b384eb642ecb
